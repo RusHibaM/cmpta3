42072a35c0f7d15b3a9df6192c9bea4ef8a7ce05
RusHibaM <zcgood567@sina.com>
2015-11-12 20:29:58 -0800

    bug in printer fixed

--- Shooter.cpp
+++ Shooter.cpp
@@ -177,16 +177,18 @@ void Printer(int rate)
      */
     struct timeval finish;
     int time_passed;
-    while(print_flag == 1){
-        gettimeofday(&finish, 0);
-        time_passed = (finish.tv_sec - start.tv_sec) * 1000000 + finish.tv_usec - start.tv_usec;
-        if(time_passed % (1000000/rate) != 0){
-            print_flag = 0;
-            continue;
-        }else{
-            sleep(1);
-            Gallery->Print();
-            print_flag = 0;
+    while(1){
+        if(print_flag == 1){
+            gettimeofday(&finish, 0);
+            time_passed = (finish.tv_sec - start.tv_sec) * 1000000 + finish.tv_usec - start.tv_usec;
+            if(time_passed % (1000000/rate) != 0){
+                print_flag = 0;
+                continue;
+            }else{
+                sleep(1);
+                Gallery->Print();
+                print_flag = 0;
+            }
         }
     }
 
