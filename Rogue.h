7e7214e78eedd0bafe6ecf2ecc5b64447d3e8116
RusHibaM <zcgood567@sina.com>
2015-11-14 13:25:27 -0800

    125

--- Rogue.h
+++ Rogue.h
@@ -117,6 +117,43 @@ public:
     int Success; // Rate/s of lanes shot by ROGUE
 };
 
+class My_Lock
+{
+public:
+    void lock_init(){if_lock = 0;};
+    int check_lock();
+    int set_lock();
+    int release_lock();
+    
+private:
+    int if_lock;      /* If the lanes are locked */
+};
+
+int My_Lock::check_lock()
+{
+    return if_lock;   /* If locked return 1, if not return 0 */
+}
+
+int My_Lock::set_lock()
+{
+    if (if_lock == 1) {  /* The lanes are already locked, lock failed */
+        return 0;
+    }else{
+        if_lock = 1;     /* The lanes are not locked, lock the lanes */
+        return 1;
+    }
+}
+
+int My_Lock::release_lock()
+{
+    if (if_lock == 0) {  /* The lanes are not locked, release failed */
+        return 0;
+    }else{
+        if_lock = 0;     /* Release lock successfully */
+        return 1;
+    }
+}
+
 #ifdef ROGUECOARSECLEANER
 
 class Cleaner
