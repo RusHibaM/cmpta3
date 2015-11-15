26cd35c3ed6c2d70b4a9af4a069ed5748d54590a
RusHibaM <zcgood567@sina.com>
2015-11-14 16:06:15 -0800

    rtm version 4

--- tsx-tools/rtm.cpp
+++ tsx-tools/rtm.cpp
@@ -95,6 +95,7 @@ void ShooterAction(int rate,Color PlayerColor){
         }else{
             nretries++;
         }
+        cout<<"Status is "<<status<<endl;
         cout<<"This the "<<nretries<<"th try."<<endl;
     }
 }
